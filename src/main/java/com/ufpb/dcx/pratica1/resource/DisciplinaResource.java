0075b368d2d76bd56057fa2f97664a4069a936a6
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2019-12-08 12:37:43 -0300

    HTTP 200(OK)

--- src/main/java/com/ufpb/dcx/pratica1/resource/DisciplinaResource.java
+++ src/main/java/com/ufpb/dcx/pratica1/resource/DisciplinaResource.java
@@ -6,8 +6,10 @@ import com.ufpb.dcx.pratica1.model.Disciplina;
 import com.ufpb.dcx.pratica1.repository.DisciplinaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,6 +24,7 @@ public class DisciplinaResource {
   private DisciplinaRepository disciplinaRepository;
 
   @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public List<Disciplina> getDisciplinas(){
     return disciplinaRepository.findAll();
   }

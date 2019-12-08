e1945acca9c876d412328eaaa45ae41f0f373981
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2019-12-08 16:24:14 -0300

    GET Disciplina By ID

--- src/main/java/com/ufpb/dcx/pratica1/repository/DisciplinaRepository.java
+++ src/main/java/com/ufpb/dcx/pratica1/repository/DisciplinaRepository.java
@@ -1,13 +1,16 @@
 package com.ufpb.dcx.pratica1.repository;
 
 import com.ufpb.dcx.pratica1.model.Disciplina;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * DisciplinaRepository
  */
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>{
+
+@Service
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>, CrudRepository<Disciplina, Long>{
 
   
 }
--- src/main/java/com/ufpb/dcx/pratica1/resource/DisciplinaResource.java
+++ src/main/java/com/ufpb/dcx/pratica1/resource/DisciplinaResource.java
@@ -1,17 +1,24 @@
 package com.ufpb.dcx.pratica1.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletResponse;
 import com.ufpb.dcx.pratica1.model.Disciplina;
 import com.ufpb.dcx.pratica1.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * DisciplinaResource
@@ -34,9 +41,22 @@ public class DisciplinaResource {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Disciplina inserirCategoria(@RequestBody Disciplina disciplina){
-    return disciplinaRepository.save(disciplina);
+  public ResponseEntity<Disciplina> inserirCategoria(@RequestBody Disciplina disciplina, HttpServletResponse response){
+    Disciplina disciplinaSalva = disciplinaRepository.save(disciplina);
+
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{idDisciplina}")
+              .buildAndExpand(disciplinaSalva.getId()).toUri();
+    response.setHeader("Location", uri.toASCIIString());
+
+    return ResponseEntity.created(uri).body(disciplinaSalva);
   }
 
-  
+  @GetMapping("/{idDisciplina}")
+  public Disciplina buscarDisciplinaPeloId(@PathVariable long idDisciplina){
+    return disciplinaRepository.findById(idDisciplina)
+        .orElseThrow(() -> new EntityNotFoundException());
+  }
+
+
+
 }
